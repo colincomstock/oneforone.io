71da0e6b7d203e38aeb6e62d999a68ac056cc8df
colincomstock <95597475+colincomstock@users.noreply.github.com>
2025-01-08 08:50:03 -0800

    Started Building UserGuide Component
    Very beginning work on the user guide

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,14 +1,19 @@
+import UserGuide from "./userGuide.jsx"
+
 export default function Song(props){
 
     return(
-        <div className="song">
-            <div className={props.hideShow ? "song-album-art" : "song-album-art hidden-container"}>
-                {props.hideShow && <img src={props.albumArt}/>}
+        <div className="top-container">
+            <div className="song">
+                <div className={props.hideShow ? "song-album-art" : "song-album-art hidden-container"}>
+                    {props.hideShow && <img src={props.albumArt}/>}
+                </div>
+                <div className="song-title">
+                    <h3>song title</h3>
+                    <h1 className={props.hideShow ? "show" : "hidden"}>{props.songName}</h1>
+                </div>
             </div>
-            <div className="song-title">
-                <h3>song title</h3>
-                <h1 className={props.hideShow ? "show" : "hidden"}>{props.songName}</h1>
-            </div>
+            <UserGuide />
         </div>
     )
 }
---
+++ src/components/userGuide.jsx
@@ -0,0 +1,14 @@
+export default function UserGuide() {
+    return(
+        <div className="user-guide">
+            <span>nice to meet you. <br />
+            someone you've never met left something 
+            for you to listen to. like most things in life, you gotta give 
+            something to get something. try pasting a link to a song you want
+            to leave for the next person in the box above. once you're ready to
+            exchange, go ahead and click the 'exchange' button. we'll give you
+            something new to listen to.
+            </span>
+        </div>
+    )
+}
