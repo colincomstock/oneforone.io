b7d4cff02856b07643521d44a705c08b9a89e440
colincomstock <95597475+colincomstock@users.noreply.github.com>
2025-01-14 16:02:44 -0800

    Small change in wording
    Attempted to make the background react dynamically to the images fetched from Spotify, ran into issues that would be difficult to surmount with CORS. Want the small change in wording I did outside of that to stick

--- src/components/userGuide.jsx
+++ src/components/userGuide.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default function UserGuide({ messageState }) {
     const welcomeMessage = "nice to meet you.\nthe last person who was here left a song for you to listen to. if you want to hear it, you'll have to leave something for the person who comes after you. try pasting a link to a song in the box above. ⬆️"
-    const songPreviewMessage = "awesome,\nbelow is a preview of what the next person will get when they come here to exchange songs. we've also grabbed some information about the artist, and some of their other top songs. when you're ready to leave this here for the next person, go ahead and click exchange."
+    const songPreviewMessage = "nice choice,\nbelow is a preview of what the next person will get when they come here to exchange songs. we've also grabbed some information about the artist, and some of their other top songs. when you're ready to leave this here for the next person, go ahead and click exchange."
     const exchangeMessage = "and there it is,\nbelow is the song that was left for you by the last user. just like what you left, we've also gathered some information for you about the artist, and some of their top songs. enjoy listening to the song that was left for you, and maybe check out some of their other work. you're welcome to come back here whenever you want and exchange again."
 
     const [currentMessage, setCurrentMessage] = React.useState('')
